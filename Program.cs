0d9eb942aa38e85a2bff74a44633032c7ab15c4e
swvy7 <menodoodio916@gmail.com>
2026-04-26 00:09:29 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        await IPGeolocator();
+    }
+
+    static async Task IPGeolocator()
+    {
+        Console.Write("Enter IP address: ");
+        string ip = Console.ReadLine();
+
+        string url = $"http://ip-api.com/json/{ip}";
+
+        using HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(url);
+
+        JsonDocument doc = JsonDocument.Parse(response);
+        JsonElement root = doc.RootElement;
+
+        Console.WriteLine("\n=== IP GEOLOCATION ===\n");
+        Console.WriteLine($"IP       : {root.GetProperty("query").GetString()}");
+        Console.WriteLine($"Country  : {root.GetProperty("country").GetString()}");
+        Console.WriteLine($"Region   : {root.GetProperty("regionName").GetString()}");
+        Console.WriteLine($"City     : {root.GetProperty("city").GetString()}");
+        Console.WriteLine($"ISP      : {root.GetProperty("isp").GetString()}");
+        Console.WriteLine($"Lat      : {root.GetProperty("lat").GetDouble()}");
+        Console.WriteLine($"Lon      : {root.GetProperty("lon").GetDouble()}");
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,9 @@
+This code is all mine (swvy7)
+
+You can use this for whatever you want, im just not responsible for it.
+
+And no this isnt a virus, im letting you read the code for a reason, you can search or ask chatgpt for what the code means if you dont know any of this.
+
+How to use:
+
+Just put the IP when it asks for it and see the magic
